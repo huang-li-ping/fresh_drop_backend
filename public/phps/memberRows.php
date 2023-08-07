01dda379b79aa8820dbe2b806d55a2de3088dba5
edwardtsai54398 <fashion.donk@gmail.com>
2023-08-07 13:38:28 +0800

    php全域路徑/ 撈會員資料

---
+++ public/phps/memberRows.php
@@ -0,0 +1,16 @@
+<?php
+header('Access-Control-Allow-Origin:*');
+
+try{
+    require_once("connect_chd102g2.php");
+
+    $sql = "select cus_no, cus_name, cus_email, phone, birth, address
+            from customer";
+    $member = $pdo->query($sql);
+    $memberRows = $member->fetchAll(PDO::FETCH_ASSOC);
+    // echo json_encode(["連線成功"]);
+    echo json_encode($memberRows);
+}catch(Exception $e){
+    echo json_encode(["連線失敗"]);
+}
+?>
