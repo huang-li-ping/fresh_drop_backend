a55ebbc4818384e40c8fa62140b85e0e853fd980
bob821229 <blue821229@gmail.com>
2023-08-10 19:42:28 +0800

    FAQ GAME串資料庫

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -29,17 +29,21 @@
             <tbody>
                 <tr v-for="(item, index) in showData" :key="index">
 
-                    <td>{{ item.id }}</td>
+                    <td>{{ item.result_no }}</td>
                     <td>{{ item.personality }}</td>
                     <td>
                         <select name="" id="" style="width: 200px; border-radius: 20px; padding: 4px;">
-                            <option value="" v-for="(proItem, proIndex) in item.productLists" :key="proIndex">
-                                {{ proItem.name }}
-                            </option>
+                            <option>{{ item.dish1_recipe }}</option>
+                            <option>{{ item.dish2_recipe }}</option>
+                            <option>{{ item.dish3_recipe }}</option>
+                            <option>{{ item.dish4_recipe }}</option>
                         </select>
                     </td>
-                    <td>{{ truncateText(item.txt, 6) }}</td>
-                    <td>{{ item.state }}</td>
+                    <td>{{ truncateText(item.content) }}</td>
+                    <td>
+                        <span v-if="item.status == 0">未套用</span>
+                        <span v-if="item.status == 1">套用</span>
+                    </td>
                     <td><button class="btn btn-outline-primary btn-sm" @click="openModal(item)">查閱</button></td>
                 </tr>
             </tbody>
@@ -54,25 +58,29 @@
     <div class="show_modal d-flex flex-column align-items-start gap-2" v-if="showModal">
 
         <label for="">狀態:
-            <select v-model="newData.state">
-                <option value="套用">套用</option>
-                <option value="未套用">未套用</option>
+            <select v-model="newData.status">
+                <option value="1">套用</option>
+                <option value="0">未套用</option>
             </select>
         </label>
 
-        <label for="id">結果編號:<input type="text" :value="newData.id || gameIdNum()" id="id" disabled="disabled"></label>
+        <label for="id">結果編號:<input type="text" :value="newData.result_no || gameIdNum()" id="id"
+                disabled="disabled"></label>
 
         <label for="personality">人格種類: <input type="text" :value="newData.personality" id="personality"> </label>
 
-        <label for="" v-for="(item, index) in newData.productLists" :key="index">推薦菜色{{ index + 1 }}:
-            <select v-model="item.name">
-                <option v-for="(item, index) in productName" :key="index" :value="item.name">{{ item.name }}</option>
+        <label for="" v-for="index in 4" :key="index">推薦菜色{{ index }}:
+            <select :value="newData['dish' + index + '_recipe']" @input="updateDishRecipe(index, $event.target.value)">
+                <option :value="newData.dish1_recipe">{{ newData.dish1_recipe }}</option>
+                <option :value="newData.dish2_recipe">{{ newData.dish2_recipe }}</option>
+                <option :value="newData.dish3_recipe">{{ newData.dish3_recipe }}</option>
+                <option :value="newData.dish4_recipe">{{ newData.dish4_recipe }}</option>
             </select>
         </label>
 
 
         <label for="txt" style="display: flex;">分析結果:
-            <textarea name="" id="txt" cols="30" rows="6">{{ newData.txt }}</textarea>
+            <textarea name="" id="txt" cols="30" rows="6">{{ newData.content }}</textarea>
         </label>
 
         <button class="delete">刪除</button>
@@ -101,7 +109,7 @@ export default {
                 id: null,
                 personality: ' ',
                 txt: ` `,
-                state: "未套用",
+                status: '未套用',
                 productLists: [
                     {
                         name: " ",
@@ -130,344 +138,7 @@ export default {
             searchInput: '',
             searchResult: [],
             colTitle: ["結果編號", "人格種類", "推薦菜色", "分析結果", "狀態", ""],
-            result: [
-                {
-                    id: 1,
-                    personality: '冒險家',
-                    txt: `分析原因：對新奇和刺激的渴望使你喜歡嘗試不同的味道和料理、你尋求新的口味體驗、並享受冒烹飪過程。`,
-                    state: "套用",
-                    productLists: [
-                        {
-                            name: "西班牙海鮮燉飯",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "阿根廷燉牛肉",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "墨西哥辣味雞肉湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "泰式生菜包",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 2,
-                    personality: '安逸享受者',
-                    txt: '分析原因：你對舒適和享受的追求使你喜歡選擇家常菜和溫和口味的食物。你尋求平衡和放鬆，享受那種讓你感到舒服和滿足的味道。',
-                    state: "套用",
-                    productLists: [
-                        {
-                            name: "日本櫻花蝦天婦羅",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "奶油啤酒蛤蠣",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "馬來西亞椰奶雞湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "巴西凱撒沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 3,
-                    personality: '創意人格',
-                    txt: '分析原因：你的豐富創意和熱情驅使你追求獨特的飲食體驗。你喜歡嘗試新穎的料理和特色小吃，並將食物視為藝術和表達自我的方式。',
-                    state: "套用",
-                    productLists: [
-                        {
-                            name: "塔香茄子",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "中華彗星炒飯",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "法國洋蔥湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "印度瑪撒拉薯仔沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 4,
-                    personality: '社交達人',
-                    txt: '分析原因：你喜歡社交和人際交往，食物在社交場合中扮演重要角色。你喜歡分享美食，享受小吃和下午茶點等輕鬆的社交餐點。',
-                    state: "套用",
-                    productLists: [
-                        {
-                            name: "泡椒炒鮮魚",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "希臘烤羊肉",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "意大利米蘭湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "糖漬番茄",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 5,
-                    personality: '健康控',
-                    txt: '分析原因：你對健康和營養的關注使你傾向選擇健康沙拉、素食料理和天然有機食物。你重視身體健康和營養均衡的飲食習慣。',
-                    state: "套用",
-                    productLists: [
-                        {
-                            name: "滑嫩番茄蛋",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "麻婆豆腐",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "泰式酸辣湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "中東麥麩沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 6,
-                    personality: '情感探索者',
-                    txt: '分析原因：你對情感和內心探索感興趣，食物對你來說是情感療癒的一部分。你喜歡享受舒緩心情的甜點和心靈療癒的食物，並創造溫馨的烹飪體驗。',
-                    state: "套用",
-                    productLists: [
-                        {
-                            name: "越南河粉湯",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "法國紅酒燉雞",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "意大利肉醬千層麵",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "加拿大蔓越莓野菜沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 7,
-                    personality: '冒險家',
-                    txt: `分析原因：對新奇和刺激的渴望使你喜歡嘗試不同的味道和料理、你尋求新的口味體驗、並享受冒烹飪過程。`,
-                    state: "未套用",
-                    productLists: [
-                        {
-                            name: "西班牙海鮮燉飯",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "阿根廷燉牛肉",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "墨西哥辣味雞肉湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "泰式生菜包",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 8,
-                    personality: '安逸享受者',
-                    txt: '分析原因：你對舒適和享受的追求使你喜歡選擇家常菜和溫和口味的食物。你尋求平衡和放鬆，享受那種讓你感到舒服和滿足的味道。',
-                    state: "未套用",
-                    productLists: [
-                        {
-                            name: "日本櫻花蝦天婦羅",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "奶油啤酒蛤蠣",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "馬來西亞椰奶雞湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "巴西凱撒沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 9,
-                    personality: '創意人格',
-                    txt: '分析原因：你的豐富創意和熱情驅使你追求獨特的飲食體驗。你喜歡嘗試新穎的料理和特色小吃，並將食物視為藝術和表達自我的方式。',
-                    state: "未套用",
-                    productLists: [
-                        {
-                            name: "塔香茄子",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "中華彗星炒飯",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "法國洋蔥湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "印度瑪撒拉薯仔沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 10,
-                    personality: '社交達人',
-                    txt: '分析原因：你喜歡社交和人際交往，食物在社交場合中扮演重要角色。你喜歡分享美食，享受小吃和下午茶點等輕鬆的社交餐點。',
-                    state: "未套用",
-                    productLists: [
-                        {
-                            name: "泡椒炒鮮魚",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "希臘烤羊肉",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "意大利米蘭湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "糖漬番茄",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 11,
-                    personality: '健康控',
-                    txt: '分析原因：你對健康和營養的關注使你傾向選擇健康沙拉、素食料理和天然有機食物。你重視身體健康和營養均衡的飲食習慣。',
-                    state: "未套用",
-                    productLists: [
-                        {
-                            name: "滑嫩番茄蛋",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "麻婆豆腐",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "泰式酸辣湯",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "中東麥麩沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-                {
-                    id: 12,
-                    personality: '情感探索者',
-                    txt: '分析原因：你對情感和內心探索感興趣，食物對你來說是情感療癒的一部分。你喜歡享受舒緩心情的甜點和心靈療癒的食物，並創造溫馨的烹飪體驗。',
-                    state: "未套用",
-                    productLists: [
-                        {
-                            name: "越南河粉湯",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "法國紅酒燉雞",
-                            category: "主菜",
-                            amount: 1,
-                        },
-                        {
-                            name: "意大利肉醬千層麵",
-                            category: "湯品",
-                            amount: 1,
-                        },
-                        {
-                            name: "加拿大蔓越莓野菜沙拉",
-                            category: "沙拉",
-                            amount: 1,
-                        }
-                    ]
-                },
-            ],
+
             productName: [
                 { name: "滑嫩番茄蛋" },
                 { name: "奶油啤酒蛤蠣" },
@@ -502,19 +173,20 @@ export default {
                 { name: "越南涼拌牛肉" },
                 { name: "加拿大蔓越莓野菜沙拉" },
                 { name: "日式涼拌海帶絲" },
-            ]
+            ],
+            gameData: []
         };
     },
     methods: {
         searchIdOrPhone() {
             console.log(this.searchInput);
             if (this.searchInput == '') {
-                this.searchResult = this.result
+                this.searchResult = this.gameData
             }
-            let stateResult = this.result.filter(item => {
-                return item.state.includes(this.searchInput)
+            let stateResult = this.gameData.filter(item => {
+                return item.status.includes(this.searchInput)
             })
-            let personalityResult = this.result.filter(item => {
+            let personalityResult = this.gameData.filter(item => {
                 return item.personality.includes(this.searchInput)
             })
             if (stateResult.length > 0) {
@@ -527,9 +199,10 @@ export default {
             this.showData = data
         },
         //控制顯示字數 多的用"..."省略
-        truncateText(text, length) {
-            if (text.length > length) {
-                return text.slice(0, length) + '...';
+        truncateText(text) {
+            if (text && text.length > 6) {
+                // 檢查 text 是否存在並且長度大於 6
+                return text.slice(0, 6) + "...";
             }
             return text;
         },
@@ -547,12 +220,35 @@ export default {
             this.showModal = false;
         },
         gameIdNum() {
-            const gameIdNum = this.result.length + 1;
+            const gameIdNum = this.gameData.length + 1;
             return gameIdNum;
+        },
+        //取得資料庫資料
+        getGameData() {
+            let url = `${this.$url}gameRows.php`
+            this.axios.get(url).then(res => {
+                this.gameData = res.data
+            }).catch(err => {
+                console.log(err);
+            })
+        },
+        updateDishRecipe(index, value) {
+            this.newData['dish' + index + '_recipe'] = value;
         }
     },
     created() {
-        this.searchResult = this.result
+        this.searchResult = this.gameData
+    },
+    watch: {
+        gameData: {
+            handler: function () {
+                this.searchResult = this.gameData
+            },
+            deep: true
+        },
+    },
+    mounted() {
+        this.getGameData()
     },
 
 
