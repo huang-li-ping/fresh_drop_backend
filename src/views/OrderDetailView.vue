8ad7db891eb9a6a87796b0f7754d615d912a3002
edwardtsai54398 <fashion.donk@gmail.com>
2023-08-15 19:50:10 +0800

    訂單明細資料BUG抓不到

--- src/views/OrderDetailView.vue
+++ src/views/OrderDetailView.vue
@@ -17,7 +17,7 @@
                 <p>會員信箱：<span>{{ orderInfo.cus_email }}</span></p>
             </div>
         </div>
-        <div class="between" style="display: flex;">
+        <div class="between" style="display: flex;" v-if="orderInfo">
             <div class="total">
                 <p>訂單金額：<span>{{ orderInfo.total_price }}</span></p>
             </div>
@@ -31,7 +31,7 @@
                 <p>出貨狀態：<span>{{ orderInfo.week }}</span></p>
             </div>
         </div>
-        <div class="bottom" style="display: flex;">
+        <div class="bottom" style="display: flex;" v-if="orderInfo">
 
             <div class="ord_cus">
                 <p>收件人姓名：<span>{{ orderInfo.ord_cus }}</span></p>
@@ -46,11 +46,11 @@
     </div>
     <div class="order_bottom" style="display: flex;">
         <div class="deatils_box">
-            <div class="ord_details" style="display: flex;">
+            <div class="ord_details" style="display: flex;" v-if="orderList">
                 <ul class="nav nav-tabs" v-show="orderList.length > 1">
                     <li class="nav-item" v-for="n in orderList.length" :key="n">
                         <a class="nav-link" :class="{ active: currentTab === 'WEEK1' }" @click="showCurrentData(n)"
-                            href="#">
+                                href="#">
                             WEEK{{ n }}
                         </a>
                     </li>
@@ -63,8 +63,8 @@
                         <th scope="col" v-for="(item, index) in colTitle" :key="index">{{ item }}</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr v-if="orderList" v-for="(item, index) in currentData" :key="index">
+                <tbody v-if="orderList">
+                    <tr v-for="(item, index) in currentData" :key="index">
                         <td>{{ item.recipe }}</td>
                         <td>{{ item.qty }}</td>
                         <td>{{ item.status }}</td>
@@ -72,19 +72,20 @@
                 </tbody>
             </table>
         </div>
-        <div class="bill_container" v-if="billDetail">
+        <div class="bill_container" v-if="orderList">
             <p>訂單金額明細</p>
             <div class="frist">
-                <p>{{ billDetail.week }}</p>
-                <p>{{ billDetail.qty }}</p>
-                <P>{{ billDetail.amount }}</P>
+                <p v-if="orderList.length > 1">${{ orderList.length * 200 }}/週</p>
+                <p>X{{ orderList.length }}</p>
+                <p v-if="orderList.length = 1">金額：${{ orderInfo.ord_price - 80 }}</p>
+                <p v-if="orderList.length > 1">金額：${{ (orderInfo.ord_price - 80) / 0.8 }}</p>
             </div>
-            <p style="text-align: right;">{{ billDetail.sale }}</p>
-            <p style="text-align: right;">{{ billDetail.deliv_m }}</p>
-            <p style="text-align: right;">{{ billDetail.giftcard_m }}</p>
-            <p style="text-align: right;">{{ billDetail.giftcard_n }}</p>
+            <p style="text-align: right;" v-if="orderList.length > 1">優惠折抵：${{ (orderInfo.ord_price - 80) / 0.2 }}</p>
+            <p style="text-align: right;">運費:$80</p>
+            <p style="text-align: right;" v-if="orderInfo.discount_price > 0">禮物卡折抵：${{ orderInfo.discount_price }}</p>
+            <!-- <p style="text-align: right;" v-if="orderInfo.discount_price > 0">{{ orderInfo.discount_price }}</p> -->
             <hr>
-            <p style="text-align: right;">{{ billDetail.total }}</p>
+            <p style="text-align: right;">{{ orderInfo.total_price }}</p>
         </div>
     </div>
 </template>
@@ -93,76 +94,17 @@ import PageTitle from '@/components/PageTitle.vue';
 import axios from 'axios';
 export default {
     name: 'IngredientView',
-    component: {
+    components: {
         PageTitle,
     },
     data() {
         return {
-            orderInfo: [],
-            // orderInfo:
-            // {
-            //     ord_date: "2023-07-04",
-            //     ord_no: "4",
-            //     cus_name: "藍奕柯",
-            //     cus_email:"Alice_Mckenzie@fresh.com",
-            //     deliv_no: "定期配送",
-            //     total_price: "$1480",
-            //     payment: "信用卡+禮物卡",
-            //     ord_status: "處理中",
-            //     week: "WEEK1",
-            //     ord_cus: "陳小廷",
-            //     ord_phone: "0966-987654",
-            //     ord_addr: "320 桃園市中壢區哀哀路3號",
-            //     ord_credit_no: "6548-9989-6564-3221",
-            // },
+            orderInfo: null,
+            orderList: null,
+            currentData: [],
 
             currentTab: 1,
             colTitle: ["品項", "份數", "出貨狀態",],
-            currentData: [],
-            orderList: [
-                [],
-                [],
-                [],
-                [],
-            ],
-
-            // orderList: [
-            //     [
-            //         { recipe: "滑嫩蕃茄蛋", qty: "X1", status: "已出貨(2023-07-05)" },
-            //         { recipe: "蒸蛋", qty: "X1", },
-            //         { recipe: "味噌鮮魚湯", qty: "X1", },
-            //         { recipe: "塔香茄子", qty: "X1", },
-            //     ],
-            //     [
-            //         { recipe: "越南河粉湯", qty: "X1", status: "處理中" },
-            //         { recipe: "泰式生菜包", qty: "X1", },
-            //         { recipe: "麻婆豆腐", qty: "X1", },
-            //         { recipe: "巴西凱撒沙拉", qty: "X1", },
-            //     ],
-            //     [
-            //         { recipe: "滑嫩蕃茄蛋", qty: "X1", status: "處理中" },
-            //         { recipe: "蒸蛋", qty: "X1", },
-            //         { recipe: "味噌鮮魚湯", qty: "X1", },
-            //         { recipe: "塔香茄子", qty: "X1", },
-            //     ],
-            //     [
-            //         { recipe: "越南河粉湯", qty: "X1", status: "處理中" },
-            //         { recipe: "泰式生菜包", qty: "X1", },
-            //         { recipe: "麻婆豆腐", qty: "X1", },
-            //         { recipe: "巴西凱撒沙拉", qty: "X1", },
-            //     ],
-            // ],
-            billDetail: {},
-            // billDetail: {
-            //     week: "$800 /週",
-            //     qty: "X4",
-            //     amount: "金額：$3200",
-            //     sale:"8折優惠：-$640",
-            //     deliv_m: "運費：$80",
-            //     giftcard_m: "禮物卡折抵：-$1080",
-            //     giftcard_n: "禮物卡編號：01234567",
-            //     total: "總金額：$1480",
-            // }
         };
     },
     methods: {
@@ -175,52 +117,38 @@ export default {
                 try {
                     const response = await axios.post(url, { ordNo });
                     this.orderInfo = response.data;
+                console.log(response.data);
                 } catch (error) {
-                    console.error('Error fetching order detail:',error);
+                    console.error('Error fetching order detail:', error);
                 }
             }
         },
-        // async sessionOrdNo(orderNoParam) {
-        //     if (orderNoParam) {
-        //         let url = `${this.$url}ordDetail.php`;
-        //         let params = new URLSearchParams();
-        //         params.append("ordNo", orderNoParam);
-        //         try {
-        //             let response = await this.axios.post(url, params);
-        //             this.orderInfo = response.data;
-        //         } catch (error) {
-        //             console.error(error);
-        //         }
-        //     } else {
-        //         this.$router.push('/order');
-        //     }
-        // },
     },
     mounted() {
-        this.showCurrentData(1);
-        // this.sessionOrdNo(this.orderInfo.ord_no);
         const ordNo = this.$route.params.ordNo;
         if (ordNo) {
             this.fetchOrderDetail(ordNo);
         }
         console.log(this.$store.state.ordDetail);
+        this.orderList = this.$store.state.ordDetail.orderList
+        this.orderInfo = this.$store.state.ordDetail.orderInfo
+        this.showCurrentData(1);
     },
     watch: {
         "$store.state.ordDetail": {
             handler: function (newval) {
                 console.log('watch triggered', newval);
                 this.orderInfo = newval.orderInfo;
+                this.orderList = newval.orderList
+                this.showCurrentData(1);
             },
             deep: true,
         }
     },
-    components: { PageTitle }
 };
 </script>
 
 <style lang="scss">
-// @import 'bootstrap/dist/css/bootstrap.min.css';
-// @import '@/assets/scss/all.scss';
 @import "@/assets/scss/page/ingredients.scss";
 
 .details_container {
