e5e9293376395b9635e5bf536b230e4ad0dfe131
edwardtsai54398 <fashion.donk@gmail.com>
2023-08-15 20:07:15 +0800

    Merge branch 'dev' of https://github.com/huang-li-ping/fresh_drop_backend into dev
    # Conflicts:
    #	public/phps/report.php

--- public/phps/report.php
+++ public/phps/report.php
@@ -4,9 +4,8 @@ header('Access-Control-Allow-Origin:*');
 try{
     require_once("connect_chd102g2.php");
     
-    $sql = "select r.report_no, r.member_fk, r.date, r.reason, r.state, r.opinion_no, r.report_pic, o.experience
-
-            from report r left join opinion o on r.opinion_no = o.opinion_no";
+    $sql = "select r.report_no, r.member_fk, r.date, r.reason, r.state, r.opinion_no, o.experience, o.opinion_no_pic as 'report_pic'
+        from report r left join opinion o on r.opinion_no = o.opinion_no";
     $report = $pdo->query($sql);
     $reportRows = $report->fetchAll(PDO::FETCH_ASSOC);
     // echo json_encode(["連線成功"]);
